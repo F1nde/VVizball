1b98f0e581cb44683167cf8378d6c474027d68ce
Roni Jaakkola <roni.jaakkola@student.tut.fi>
2015-02-18 16:58:08 +0200

    Muutettu näppäimet vaihdettaviksi, lisätty painovoiman vaihdon estäminen kunnes pelaaja osuu tasoon.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     [Range(0, 15)]
-    public float moveForce;        // The amount of force added to the object
+    public float moveForce;         // The amount of force added to the object
 
     Rigidbody2D rbody;              // Reference to the rigidbody component
+    bool canChangeGravity = true;   // Whether the player is able to change gravity
 
 	// Use this for initialization
 	void Start () 
@@ -18,36 +19,48 @@ public class PlayerController : MonoBehaviour {
 	// FixedUpdate is fixed to the framerate
 	void FixedUpdate () 
     {
-        if (Input.GetButtonDown("Gravity")) // TODO: Prevent player from changing this constantly
-            ChangeGravity();
-
 		Movement();
 	}
 
     void ChangeGravity()
     {
-        rbody.gravityScale *= -1;
+        if (canChangeGravity)
+        {
+            rbody.gravityScale *= -1;
+            canChangeGravity = false;
+        }
+
         Debug.Log("Gravity changed!");
     }
 	
 	void Movement ()
 	{
-		// WASD and regular keys included
-		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+        // PLAYER MOVEMENT INPUT
+        // =====================
+		// - WASD and regular keys included
+        // - If you want to change these, go to Edit->Project Settings->Input
+
+        if (Input.GetButton("Gravity1") || Input.GetButton("Gravity2")){
 			ChangeGravity();
 		}
-		
-		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+
+        if (Input.GetButton("Right")) {
             rbody.AddForce(Vector3.right * moveForce);
-		}
+        }
 
-		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-			ChangeGravity();
-		}
-
-		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetButton("Left")) {
             rbody.AddForce(Vector3.left * moveForce);
-		}
+        }
+
 	}
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        // Reset gravity bool if player hits a platform
+        if (collider.gameObject.tag == "Platform") {
+            Debug.Log("Player hit a platform!");
+            canChangeGravity = true;
+        }
+    }
 
 }
