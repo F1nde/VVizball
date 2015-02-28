71aff950d51c308a3a588b5b673b1bf44c5e77bd
Jaakko Husso <jaakko.husso@student.tut.fi>
2015-02-28 19:50:29 +0200

    Lisättiin peliin pausemenu joka aukeaa escapea painamalla. Menun saa suljettua Resume-napista tai painamalla escape uudestaan. Pelistä voi poistua (ei editorissa) painamalla exit. Pelin fysiikat pysähtyvät kun peli pausetaan.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	public GameObject Panel;
+	private bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+		Panel.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && !paused) {
+			Panel.gameObject.SetActive (true);
+			Time.timeScale = 0;
+			paused = true;
+		} else if (Input.GetKeyDown (KeyCode.Escape) && paused) {
+			resume ();
+		}
+	}
+
+	public void resume() {
+		Panel.gameObject.SetActive(false);
+		Time.timeScale = 1;
+		paused = false;
+	}
+
+	public void exit() {
+		Application.Quit ();
+	}
+}
