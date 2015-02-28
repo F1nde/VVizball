9bb07d7c96987eae7de6e46b1ce9bba91efaeb45
Jaakko Husso <jaakko.husso@student.tut.fi>
2015-02-28 18:47:51 +0200

    Timer now goes down from 100 seconds and is properly anchored to top left corner. Need to discuss how score actually works in this game.

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class ScoreDisplay : MonoBehaviour {
 
 	Text score;
-	private double timer = 0.0;
+	private double timer = 100.0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +15,7 @@ public class ScoreDisplay : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
+		timer -= Time.deltaTime;
 		score.text = "Time: " + timer.ToString("0.00") + "s";
 	}
 }
