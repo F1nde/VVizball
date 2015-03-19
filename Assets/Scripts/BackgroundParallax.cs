c750db6879ba8fa28124aef1635fa7412ab8532e
Jaakko Husso <jaakko.husso@student.tut.fi>
2015-03-19 21:27:31 +0200

    Muokattiin hiukan parallex scrollingia testinä, nyt taustan indeksi määrää liikkeen määrän

--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -20,9 +20,10 @@ public class BackgroundParallax : MonoBehaviour
 	{
 		var paralax = (lastPosition.x - Player.position.x) * parallexScale;
 
+		//Make elements with higher index move more (closer to camera)
 		for(var i = 0; i < Backgrounds.Length; ++i)
 		{
-			var backgroundTargetPositionX = Backgrounds[i].position.x + paralax;
+			var backgroundTargetPositionX = Backgrounds[i].position.x + paralax*i;
 			Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPositionX, Backgrounds[i].position.y, Backgrounds[i].position.z);
 			Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, backgroundTargetPosition, smoothing * Time.deltaTime);
 		}
