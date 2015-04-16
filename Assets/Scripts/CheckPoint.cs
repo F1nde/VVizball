855f0198371cd23804b012dc054cbe05698d49a5
Jaakko Husso <jaakko.husso@student.tut.fi>
2015-04-16 21:30:41 +0300

    Lisättiin syttyvä ja sammuva lamppu

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,5 +1,5 @@
 ﻿// TIE-21106 Software Engineering Methodology, 2015
-// Roni Jaakkola, Marko Tuominen, Noora Männikkö, 2015
+// Roni Jaakkola, Marko Tuominen, Jaakko Husso, Noora Männikkö, 2015
 
 using UnityEngine;
 using System.Collections;
@@ -7,11 +7,17 @@ using System.Collections;
 public class CheckPoint : MonoBehaviour {
 
 	public LevelManager Lmanager;
-
+	public SpriteRenderer renderer;
+	public Sprite[] sprites;
 	public Transform playerSpawnPoint;
-	
+
+	void Start() {
+		renderer.sprite = sprites[0];//lamp is off
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		renderer.sprite = sprites[1];//Turn lamp on
 		Debug.Log("Checkpoint!");
 		Lmanager.NewCheckpoint(playerSpawnPoint.position);
 	}
