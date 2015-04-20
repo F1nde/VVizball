6e85ba5474ae09ce1abfd964e7d66cbc631757d1
Jaakko Husso <jaakko.husso@student.tut.fi>
2015-04-20 14:31:39 +0300

    Korjattiin pari bugia vihollisista ja luotiin uusi vihollistyyppi, joka kykenee liikkumaan monien pisteiden väliä ja nopeus on säädettävissä.

---
+++ Assets/Scripts/AdvancedEnemy.cs
@@ -0,0 +1,46 @@
+// TIE-21106 Software Engineering Methodology, 2015
+// Roni Jaakkola, Marko Tuominen, Jaakko Husso, Noora Männikkö, 2015
+
+using UnityEngine;
+using System.Collections;
+
+public class AdvancedEnemy : MonoBehaviour
+{
+	bool state;
+
+	// Use this for initialization
+	void Start ()
+	{
+		state = true;
+	}
+
+	// Remove when dead
+	public void killEnemy()
+	{
+		renderer.enabled = false;
+		collider2D.enabled = false;
+	}
+
+	//Hitting another enemy kills the player!
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player"){
+			Debug.Log ("Player hit an enemy!");
+			if (PlayerController.hitEnemy ()) {
+				killEnemy ();
+			}
+			state = false;
+		}
+	}
+	
+	public void Reset()
+	{
+		if (!state)
+		{
+			renderer.enabled = true;
+			collider2D.enabled = true;
+			state = true;
+		}
+	}
+}
+
---
+++ Assets/Scripts/MoveEnemyMultiplePoints.cs
@@ -0,0 +1,67 @@
+// TIE-21106 Software Engineering Methodology, 2015
+// Roni Jaakkola, Marko Tuominen, Jaakko Husso, Noora Männikkö, 2015
+
+using UnityEngine;
+using System.Collections;
+
+public class MoveEnemyMultiplePoints : MonoBehaviour
+{
+	//At least 2 points!
+	public Transform[] points;
+	public Transform enemy;
+	public float speed;
+
+	Transform destination;
+	int currentPoint = 0;
+	Vector3 direction;
+
+	bool state;
+
+	// Use this for initialization
+	void Start ()
+	{
+		state = true;
+		currentPoint = 0;
+		//Always sets destination to next point
+		setDestination ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//enemy.rigidbody2D.MovePosition(enemy.position + direction * speed * Time.fixedDeltaTime);
+
+		enemy.Rotate (Vector3.forward * Time.deltaTime * 360);
+		enemy.Translate (new Vector3(direction.x * speed * Time.fixedDeltaTime, direction.y * speed * Time.fixedDeltaTime, 0), Space.World);
+		if (Vector3.Distance (enemy.position, destination.position) < speed * Time.fixedDeltaTime)
+		{
+			setDestination ();
+		}
+
+	}
+
+	// Set target location
+	void setDestination()
+	{
+		++currentPoint;
+		destination = points[currentPoint % points.Length];
+		direction = (destination.position - enemy.position).normalized;
+	}
+
+	// Change direction - not used??
+	void changeDirection()
+	{
+		setDestination ();
+	}
+
+	public void Reset()
+	{
+		if (!state)
+		{
+			currentPoint = points.Length;
+			setDestination();
+			state = true;
+		}
+	}
+}
+
